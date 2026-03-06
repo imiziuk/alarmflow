f157c9ec8c7ecf24b631550fa7eff70c5b491deb
Tristen Hale <tahale1@csuchico.edu>
2026-03-05 23:36:54 -0800

    created Sound class for alarm sound, start/stop with alarm

--- AlarmApp/App.tsx
+++ AlarmApp/App.tsx
@@ -1,30 +1,96 @@
-import React, { useState, useEffect } from 'react';
+// libraries
+import React from 'react';
+
+import {
+    useState,
+    useEffect
+} from 'react';
 import {
   View,
   Text,
   Button,
   Alert,
   StyleSheet,
- } from 'react-native';
+} from 'react-native';
 
 import DateTimePicker from '@react-native-community/datetimepicker';
 
-export default function App() {
-  const [alarmTime, setAlarmTime] = useState<Date | null>(null);
-  const [showPicker, setShowPicker] = useState(false);
-  const [alarmSet, setAlarmSet] = useState(false);
 
-  // check every second
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (!alarmTime || !alarmSet) return;
+// local
+import SOUND from "./Sound.tsx"
+import {
+  SECOND
+} from "./Time.tsx"
 
-      const now = new Date();
-      if (now.getHours() === alarmTime?.getHours() && now.getMinutes() === alarmTime.getMinutes() && now.getSeconds() === 0) {
-        Alert.alert("Wake up!!!");
-        setAlarmSet(false);
+
+// FUNCTIONS
+function check_time(current_time: Date, alarm_time: Date): boolean {
+  let CT: Date = current_time;    // current time
+  let AT: Date = alarm_time;      // alarm time
+
+  // alarm should go off when both times match
+  if(CT.getHours() === AT.getHours()){
+      if(CT.getMinutes() === AT.getMinutes()){
+          return true;
       }
-    }, 1000);
+  }
+
+  // the alarm should NOT go off when they are different
+  return false;
+}
+
+
+
+// MAIN APP
+export default function App() {
+  const [alarmTime, setAlarmTime]   = useState<Date | null>(null);
+  const [showPicker, setShowPicker] = useState(false);
+  const [alarmSet, setAlarmSet]     = useState(false);
+
+
+  // setup alarm sound
+  let alarm_sound = new SOUND("mgs_codec.mp3");
+
+
+  // main loop, check every second
+  useEffect(() => {
+  const interval = setInterval(() => {
+    if (!alarmTime || !alarmSet) return;
+
+    const now = new Date();
+    if (check_time(now, alarmTime) == true) {
+      // play alarm
+      alarm_sound.play();
+
+      Alert.alert(
+        // messages
+        "Wake up!!!",
+        "",
+        // buttons
+        [
+          {
+            text: "OK",
+            onPress: () => {
+              // stop alarm
+              console.log("DEBUG] Alert - 'OK' pressed");
+              alarm_sound.stop();
+            }
+          },
+          {
+            text: "SNOOZE",
+            onPress: () => {
+              // stop alarm
+              console.log("DEBUG] Alert - 'SNOOZE' pressed");
+              alarm_sound.stop();
+            }
+          }
+        ],
+        // options
+        { cancelable: false }
+      );
+      setAlarmSet(false);
+    }
+  }, SECOND);
 
     return () => clearInterval(interval);
   }, [alarmTime, alarmSet]);
---
+++ AlarmApp/Sound.tsx
@@ -0,0 +1,88 @@
+import Sound         from "react-native-sound";
+import SystemSetting from 'react-native-system-setting'
+
+
+// ========================== //
+// SOUND
+// ========================== //
+interface SOUND_INTERFACE {
+    // object members
+    name:   string;
+    path:   string;
+    sound:  Sound;
+    volume: number;
+
+    // function members
+    load(): void;
+    play(): void;
+    stop(): void;
+}
+
+export default class SOUND implements SOUND_INTERFACE {
+    // ---------------- //
+    // members
+    // ---------------- //
+    name:   string;
+    path:   string;
+    sound:  Sound;
+    volume: number;
+
+    // ---------------- //
+    // contructor 
+    // ---------------- //
+    constructor(name: string) {
+        this.name   = name;
+        this.path   = Sound.MAIN_BUNDLE;
+        this.sound  = new Sound(this.name, this.path);
+        this.volume = 0
+    }
+
+    // ---------------- //
+    // functions
+    // ---------------- //
+    load(): void {
+        // load new sound
+        this.sound = new Sound(this.name, this.path, (error) => {
+            // error checking
+            if(error) {
+                console.log("ERROR] failed to LOAD sound", error);
+                return;
+            }
+            console.log("DEBUG] loaded sound...");
+        });
+    }
+    play(): void {
+        // check if sound is NOT currently loaded
+        if(!this.sound.isLoaded()){
+            this.load(); 
+        }
+
+        // sound setup
+        //  - using SystemSetting, the return is an async Promise (multithreaded callback)
+        //     ... which results in the 'play' feature in this code block
+        SystemSetting.getVolume("alarm").then((currentVolume: number)  => { 
+            let sys_alarm_volume: number = Number(currentVolume.toFixed(3));
+
+            // set sound parameters 
+            this.sound.setVolume(sys_alarm_volume);
+            this.sound.setNumberOfLoops(-1);
+
+            // play sound
+            this.sound.play((success) => {
+                // error checking
+                if(success) {
+                    console.log("DEBUG] playing sound");
+                }
+                else{
+                    console.log("ERROR] failed to PLAY sound");
+                }
+            });
+        }).catch((error) => {
+            console.error("ERROR] failed to get SYSTEM ALARM VOLUME", error);
+        });
+    }
+    stop(): void {
+        // stop current sound
+        this.sound.stop();
+    }
+}
